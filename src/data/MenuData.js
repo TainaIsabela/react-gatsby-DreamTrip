a81f3822903309f4967d132c35343fd4c7ba0473
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-20 23:43:53 -0300

    Creating NavBar Section

---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const menuData = [
+    {title: 'Sobre', link:"/about"},
+    {title: 'Viagens', link:"/trips"},
+    {title: 'Carreiras', link:"/careers"},
+    {title: 'Contato', link:"/contact"},
+
+]
